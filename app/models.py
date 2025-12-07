e4dcd81752f2e2ee967173111ac6365672869b28
Connor <kangruichen431@gmail.com>
2025-12-07 04:54:52 -0800

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,21 +1,28 @@
 from app import db, login_manager
 from flask_login import UserMixin
 
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(120), unique=True, nullable=False)
 
+
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
+
 
 class Bookmark(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(255))
     url = db.Column(db.String(255))
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    folder_id = db.Column(db.Integer, db.ForeignKey("folder.id"), nullable=True)
+
 
 class Folder(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+
+    bookmarks = db.relationship("Bookmark", backref="folder", lazy=True)
