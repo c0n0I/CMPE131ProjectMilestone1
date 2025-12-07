59ec76d964a11632b9e3d0cd54ead6ba1da3d473
Connor <kangruichen431@gmail.com>
2025-12-07 02:37:09 -0800

    Add BookmarkForm for managing bookmarks

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,12 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, URL
 
 class LoginForm(FlaskForm):
     username = StringField("Username", validators=[DataRequired()])
     submit = SubmitField("Log In")
+
+class BookmarkForm(FlaskForm):
+    title = StringField("Title", validators=[DataRequired()])
+    url = StringField("URL", validators=[DataRequired(), URL()])
+    submit = SubmitField("Add Bookmark")
