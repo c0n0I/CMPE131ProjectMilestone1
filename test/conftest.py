80eb3ece6f8c9ddca7de6e801f1f585498b3c4de
Connor <kangruichen431@gmail.com>
2025-12-08 16:29:35 -0800

    Create conftest.py

---
+++ test/conftest.py
@@ -0,0 +1,32 @@
+import sys
+import os
+
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+import pytest
+from app import create_app, db
+from app.models import User, Folder, Bookmark
+
+@pytest.fixture
+def test_app():
+    # Create the Flask test app
+    app = create_app()
+    app.config.update({
+        "TESTING": True,
+        "SQLALCHEMY_DATABASE_URI": "sqlite:///:memory:",
+        "WTF_CSRF_ENABLED": False
+    })
+
+    with app.app_context():
+        db.create_all()
+        
+        yield app  
+
+        db.session.remove()
+        db.drop_all()
+
+
+@pytest.fixture
+def client(test_app):
+    return test_app.test_client()
